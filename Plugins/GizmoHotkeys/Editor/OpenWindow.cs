6e96904b0cfa435e5fa5749748e1d8b0c9619507
jaewon.jeong <sunrice.china@gmail.com>
2018-05-25 16:50:32 +0800

    Open Windows  - Lighting Settings Ctrl + L  - Lighting Explorer Ctrl + Shift + L  - Frame Debugger Ctrl + F7

---
+++ Plugins/GizmoHotkeys/Editor/OpenWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using UnityEditor;
+
+public class OpenWindow : EditorWindow
+{
+	[MenuItem("Scene View/Open/Lighting Settings %L")]
+	static void OpenLightingSettings()
+	{
+		EditorApplication.ExecuteMenuItem("Window/Lighting/Settings");
+	}
+
+	[MenuItem("Scene View/Open/Light Expolorer #%L")]
+	static void OpenLightExplorer()
+	{
+		EditorApplication.ExecuteMenuItem("Window/Lighting/Light Explorer");
+	}
+	
+	[MenuItem("Scene View/Open/Frame Debugger %F7")]
+	static void OpenFrameDebugger()
+	{
+		EditorApplication.ExecuteMenuItem("Window/Frame Debugger");
+	}
+}
