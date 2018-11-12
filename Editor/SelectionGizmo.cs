05516ebfc3ce1655cbefb150d328e2b66e03646d
J1.Jeong <35213382+unity3d-kr@users.noreply.github.com>
2018-11-12 17:25:25 +0800

    Fix: Gizmo Size restore on Unity 4.7

--- Editor/SelectionGizmo.cs
+++ Editor/SelectionGizmo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Reflection;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,7 +21,12 @@ namespace J1Jeong
 			internal const string showGrid = "showGrid";
 		}
 
-		[MenuItem( "Window/Hotkeys/Gizmo Icons %g", priority=1)]
+
+#if !UNITY_5
+        static float iconSize = 0;
+        static bool use3dGizmos = false;
+#endif
+        [MenuItem( "Window/Hotkeys/Gizmo Icons %g", priority=101)]
 		public static void HideGizmoIcons()
 		{
 			Assembly asm = Assembly.GetAssembly( typeof(Editor) );
@@ -32,29 +38,40 @@ namespace J1Jeong
 
 				float nowIconSize = (float) iconSizeProperty.GetValue( asm, null );
 				if( nowIconSize > 0 ) // to hide
-				{
+                {
+#if UNITY_5
 					EditorPrefs.SetFloat(Strings.prefs_use3dGizmos, nowIconSize);
-					iconSizeProperty.SetValue( asm, 0, null );
-					
+#endif
+                    iconSize = nowIconSize;
+                    iconSizeProperty.SetValue( asm, 0, null );
+
+#if UNITY_5
 					bool use3dGizmos = (bool) use3dGizmosProperty.GetValue( asm, null );
 					EditorPrefs.SetBool(Strings.prefs_use3dGizmos, use3dGizmos);
-					use3dGizmosProperty.SetValue( asm, true, null );
+#else
+                    use3dGizmos = (bool)use3dGizmosProperty.GetValue(asm, null);
+#endif
+                    use3dGizmosProperty.SetValue( asm, true, null );
 				}
 				else // to show
-				{
+                {
+#if UNITY_5
 					float iconSize = EditorPrefs.GetFloat(Strings.prefs_iconSize);
-					if (iconSize <= 0)
+#endif
+                    if (iconSize <= 0)
 						iconSize = 0.03162277f; // Mathf.Pow(10f, -3f + 3f * 0.5f), see to Convert01ToTexelWorldSize()
 					iconSizeProperty.SetValue( asm, iconSize, null );
 
+#if UNITY_5
 					bool use3dGizmos = EditorPrefs.GetBool(Strings.prefs_use3dGizmos);
+#endif
 					use3dGizmosProperty.SetValue( asm, use3dGizmos, null );
 				}
 			}
 		}
 		
 
-		[MenuItem( "Window/Hotkeys/Show Grid %#g", priority=1 )]
+		[MenuItem( "Window/Hotkeys/Show Grid %#g", priority= 101)]
 		public static void ShowGrid()
 		{
 			Assembly asm = Assembly.GetAssembly( typeof(Editor) );
@@ -68,7 +85,7 @@ namespace J1Jeong
 		}
 
 #if UNITY_5_5_OR_NEWER
-		[MenuItem( "Window/Hotkeys/Selection Outline %h", priority=1 )]
+		[MenuItem( "Window/Hotkeys/Selection Outline %h", priority=101 )]
 		public static void HideOutline()
 		{
 			Assembly asm = Assembly.GetAssembly( typeof(Editor) );
@@ -81,7 +98,7 @@ namespace J1Jeong
 			}
 		}
 
-		[MenuItem( "Window/Hotkeys/Selection Wire %#h", priority=1 )]
+		[MenuItem( "Window/Hotkeys/Selection Wire %#h", priority=101 )]
 		public static void HideWireframe()
 		{
 			Assembly asm = Assembly.GetAssembly( typeof(Editor) );
@@ -94,72 +111,93 @@ namespace J1Jeong
 			}
 		}
 #else
-		// for Unity Old Version
-		static List<GameObject> hideObjects = new List<GameObject>();
+        // for Unity Old Version
+        static List<GameObject> hideObjects = new List<GameObject>();
 
-		[MenuItem("Window/Hotkeys/Hide WireFrame %h")]
-		static void HideWireframe()
-		{
-			bool IsAllHide = true;
+	    [MenuItem("Window/Hotkeys/Hide WireFrame %h", priority=101)]
+	    static void HideWireframe()
+	    {
+		    bool IsAllHide = true;
 
-			for (int i = 0; i < Selection.gameObjects.Length; i++)
-			{
-				GameObject s = Selection.gameObjects[i];
-				if (hideObjects.Contains(s) == false)
-				{
-					IsAllHide = false;
-					break;
-				}
-			}
+		    for (int i = 0; i < Selection.gameObjects.Length; i++)
+		    {
+                GameObject s = Selection.gameObjects[i];
+                Renderer rend = s.GetComponent<Renderer>();
 
-			// Unselected
-			for (int i = 0; i < hideObjects.Count; i++)
-			{
-				GameObject s = hideObjects[i];
-				if (s != null && s.activeSelf)
-				{
-					Renderer rend = s.GetComponent<Renderer>();
-					if (rend)
-						EditorUtility.SetSelectedWireframeHidden(rend, false); // show
-				}
-			}
-			hideObjects.Clear();
+                if (rend)
+                {
+                    if (hideObjects.Contains(s) == false)
+                    {
+                        IsAllHide = false;
+                        break;
+                    }
+                }
+                else if (Selection.gameObjects.Length == 1)
+                {
+                    Renderer[] children = s.GetComponentsInChildren<Renderer>();
+                    for (int j = 0; j < children.Length; j++)
+                    {
+                        if (hideObjects.Contains(children[i].gameObject) == false)
+                        {
+                            IsAllHide = false;
+                            break;
+                        }
+                    }
+                }
+            }
 
-			for (int i = 0; i < Selection.gameObjects.Length; i++)
-			{
-				GameObject s = Selection.gameObjects[i];
-				Renderer rend = s.GetComponent<Renderer>();
+		    // Unselected
+		    for (int i = 0; i < hideObjects.Count; i++)
+		    {
+			    GameObject s = hideObjects[i];
+			    if (s != null && s.activeSelf)
+			    {
+				    Renderer rend = s.GetComponent<Renderer>();
+				    if (rend)
+					    EditorUtility.SetSelectedWireframeHidden(rend, false); // show
+			    }
+		    }
+		    hideObjects.Clear();
 
-				if (rend)
-				{
-					if (IsAllHide)
-					{
-						EditorUtility.SetSelectedWireframeHidden(rend, false); // show
-					}
-					else
-					{
-						EditorUtility.SetSelectedWireframeHidden(rend, true); // hide
-						hideObjects.Add(s);
-					}
-				}
-				else if(Selection.gameObjects.Length == 1)
-				{
-					Renderer[] children = s.GetComponentsInChildren<Renderer>();
-					for (int j = 0; j < children.Length; j++)
-					{
-						EditorUtility.SetSelectedWireframeHidden(rend, true); // hide
-						hideObjects.Add(children[j].gameObject);
-					}
+            for (int i = 0; i < Selection.gameObjects.Length; i++)
+		    {
+			    GameObject s = Selection.gameObjects[i];
+			    Renderer rend = s.GetComponent<Renderer>();
 
-				}
-			}
-		}
+                if (rend)
+                {
+                    if (IsAllHide)
+                    {
+                        // Hide before compile.
+                        EditorUtility.SetSelectedWireframeHidden(rend, false); // show
+                    }
+                    else
+                    {
+                        EditorUtility.SetSelectedWireframeHidden(rend, true); // hide
+                        hideObjects.Add(s);
+                    }
+			    }
+			    else if(Selection.gameObjects.Length == 1)
+			    {
+                    if (IsAllHide)
+                        break;
 
-		[MenuItem("Window/Hotkeys/Hide WireFrame %h", true)]
-		static bool HideWireframeValidate()
-		{
-			return Selection.activeGameObject != null;
-		}
+                    Renderer[] children = s.GetComponentsInChildren<Renderer>();
+                    for (int j = 0; j < children.Length; j++)
+                    {
+                        EditorUtility.SetSelectedWireframeHidden(children[j], true); // hide
+                        hideObjects.Add(children[j].gameObject);
+                    }
+			    }
+		    }
+	    }
+
+	    [MenuItem("Window/Hotkeys/Hide WireFrame %h", true)]
+	    static bool HideWireframeValidate()
+	    {
+		    return Selection.activeGameObject != null;
+	    }
 #endif
 	}
 }
+#endif
